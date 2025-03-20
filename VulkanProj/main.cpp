a37f976f1add21a972b1b5566d44601289b322f0
Kevin Kim <sycodes95@gmail.com>
2025-03-19 23:31:29 -0700

    some physical device

--- VulkanProj/main.cpp
+++ VulkanProj/main.cpp
@@ -68,7 +68,7 @@ private:
 
 	void createInstance() {
 		
-		if (enableValidationLayers && !checkValidationLayerSupport) {
+		if (enableValidationLayers && !checkValidationLayerSupport()) {
 			throw std::runtime_error("validation layers requested, but not available");
 		}
 
@@ -199,8 +199,39 @@ private:
 	void initVulkan() {
 		createInstance();
 		setupDebugMessenger();
+		pickPhysicalDevice();
 	}
-	
+
+	void pickPhysicalDevice() {
+		VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+
+		uint32_t deviceCount = 0;
+		vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+
+		if (deviceCount == 0) {
+			throw std::runtime_error("failed to find GPUs with Vulkan support!");
+		}
+
+		std::vector<VkPhysicalDevice> devices(deviceCount);
+		vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+		for (const auto& device : devices) {
+			if (isDeviceSuitable(device)) {
+				physicalDevice = device;
+				break;
+			}
+		}
+
+		if (physicalDevice == VK_NULL_HANDLE) {
+			throw std::runtime_error("failed to find a suitable GPU!");
+		}
+		
+	}
+
+	bool isDeviceSuitable(VkPhysicalDevice device) {
+		return true;
+	}
+
 	void mainLoop() {
 		while (!glfwWindowShouldClose(window)) {
 			glfwPollEvents();
